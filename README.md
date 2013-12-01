fe50cb335b4e0bd969474eff8c0bbb891fec28f7
eatseng <axlinkck@aol.com>
2013-12-01 20:42:56 +0000

    Updated README and added Baseball Elimination project

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ Algorithm
 =========
 
 Computer Science Algorithm Projects
+
+Baseball Mathematical Elimination (Max Flow/Min Cut/Flow Network)
+
+	http://coursera.cs.princeton.edu/algs4/assignments/baseball.html
+	
+	Construct: A Flow Network is constructed using number of remaining games as flow edges from the source. The capacity of flow edges to the sink is constructed using the maximum number of games team under investigation (team UI) can possibly win against team n. That maximum number of wins is restricted by the number of wins team n already has. If team n's flow edge to the sink is saturated (occupying 100% flow capacity,) it means team UI will not achieve as many wins as team i; therefore, team UI is mathematically eliminated by team n.
+	
+	*Optimization: FordFulkerson algorithm is used to calculate flow within the network. It does so by looping and updating network with flow information until flow from source = flow to sink. To shorten execution time, number of looping and updating have to be minimized. This can be achieved by integrating FF algorithm into the code to allow continuous update to flow network, rearranging the team by the number of games won + remaining games in ascending order, and adding additional flow edges as teams down the list have greater number of games won + remaining games. By doing so, flow in network can be continuously updated rather than starting from scratch each time a different team is investigated.
+
+	*yet to be complete, but the current passes project spec 100%.
+
+
+The following projects uses library file algs4.jar and stdlib.jar. These library files are part of Coursera coursework created by Kevin Wayne and Robert Sedgewick.
---
+++ baseball_elimination/BaseballElimination.java
@@ -0,0 +1,267 @@
+//Edward Tseng
+//11/16/13
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BaseballElimination {
+	private int[] w, l, r;
+	private int[][] g;
+	private int num_teams;
+	private String[] teams;
+	private String prev_team;
+	private Set<String> rset;
+	private boolean peliminated;
+	
+	public static void main(String[] args) {
+		Stopwatch sw = new Stopwatch();
+		
+		BaseballElimination division = new BaseballElimination("teams4.txt");
+		for (String team : division.teams()) {
+	        if (division.isEliminated(team)) {
+	            StdOut.print(team + " is eliminated by the subset R = { ");
+	            for (String t : division.certificateOfElimination(team))
+	                StdOut.print(t + " ");
+	            StdOut.println("}");
+	        }
+	        else {
+	            StdOut.println(team + " is not eliminated");
+	        }
+	    }
+		
+		System.out.println("Resizing time: " + sw.elapsedTime() + " seconds.");
+	    /*
+		System.out.println("teams: " + division.teams());
+		for (String teams : division.teams()) {
+			System.out.println(teams);
+			System.out.println("elminated? " + division.isEliminated(teams));
+		}
+			*/
+		
+		System.out.println(division.isEliminated("Atlanta"));
+		System.out.println(division.certificateOfElimination("Atlanta"));
+		
+	}
+	
+	public BaseballElimination(String filename) {
+		// create a baseball division from given filename in format specified below
+		In inputData = new In(filename);
+
+		this.num_teams = Integer.parseInt(inputData.readLine());
+		this.w = new int[num_teams];
+		this.l = new int[num_teams];
+		this.r = new int[num_teams];
+		this.teams = new String[num_teams];
+		this.g = new int[num_teams][num_teams];
+		
+		for(int i = 0; i < num_teams; i++) {
+			String temp = inputData.readLine();
+			for(int l = 0; l < temp.length(); l++)
+				if (temp.charAt(l) != ' ') {
+					temp = temp.substring(l);
+					break;
+				}
+			String[] teamData = temp.split(" +");
+			this.teams[i] = teamData[0];
+			this.w[i] = Integer.parseInt(teamData[1]);
+			this.l[i] = Integer.parseInt(teamData[2]);
+			this.r[i] = Integer.parseInt(teamData[3]);
+			for (int j = 0; j < num_teams; j++) {
+				this.g[i][j] = Integer.parseInt(teamData[j + 4]);
+			}
+		}
+	}
+	
+	public int numberOfTeams() {
+		// number of teams
+		return num_teams;
+	}
+	
+	public Iterable<String> teams() {
+		// all teams
+		return Arrays.asList(teams);
+	}
+	
+	public int wins(String team) {
+		// number of wins for given team
+		for (int i = 0; i < numberOfTeams(); i++)
+			if (teams[i].equals(team))
+				return w[i];
+		throw new java.lang.IllegalArgumentException("No such team");
+	}
+	
+	public int losses(String team) {
+		// number of losses for given team
+		for (int i = 0; i < numberOfTeams(); i++)
+			if (teams[i].equals(team))
+				return l[i];
+		throw new java.lang.IllegalArgumentException("No such team");
+	}
+	
+	public int remaining(String team) {
+		// number of remaining games for given team
+		for (int i = 0; i < numberOfTeams(); i++)
+			if (teams[i].equals(team))
+				return r[i];
+		throw new java.lang.IllegalArgumentException("No such team");
+	}
+	
+	public int against(String team1, String team2) {
+		// number of remaining games between team1 and team2
+		int x = -1, y = -1;
+		for (int i = 0; i < numberOfTeams(); i++)
+			if (teams[i].equals(team1)) {
+				x = i;
+				break;
+			}
+		for (int i = 0; i < numberOfTeams(); i++)
+			if (teams[i].equals(team2)) {
+				y = i;
+				break;
+			}
+		if (y < 0 || x < 0)
+			throw new java.lang.IllegalArgumentException("No such team");
+		return g[x][y];
+	}
+	
+	public boolean isEliminated(String team) {
+		// is given team eliminated?
+		if (team.equals(prev_team))
+			return peliminated; 
+	
+		//get current team index and store it in x
+		int x = -1;
+		for (int i = 0; i < numberOfTeams(); i++)
+			if (teams[i].equals(team))
+				x = i;
+		
+		if (x == -1)
+			throw new java.lang.IllegalArgumentException("No such team");
+
+		prev_team = team;
+		this.rset = new HashSet<String>();
+		peliminated = false;
+		
+		//determine if current team is trivially eliminated
+		for (String t : teams)
+			if (w[x] + r[x] < wins(t)) {
+				rset.add(t);
+				peliminated = true;
+			}
+	
+		if (peliminated)			
+			return true;
+		
+		//if not trivially elminated
+		int v = 2 + num_teams;		//track number of vertices
+		for(int i = 0; i < num_teams; i++) 
+			for (int j = i + 1; j < num_teams; j++)
+				if (g[i][j] != 0)
+					v++;
+		
+		FlowNetwork fn = new FlowNetwork(v);
+		
+		//connect start to games to teams
+		int k = 0;
+		for(int i = 0; i < num_teams; i++) 
+			for (int j = i + 1; j < num_teams; j++)
+				if (g[i][j] != 0) {
+					fn.addEdge(new FlowEdge(num_teams, num_teams + k + 2, g[i][j]) );
+					fn.addEdge(new FlowEdge(num_teams + k + 2, i, Double.POSITIVE_INFINITY));
+					fn.addEdge(new FlowEdge(num_teams + k + 2, j, Double.POSITIVE_INFINITY));
+					k++;
+				}
+		
+		//connect teams to end
+		for(int i = 0; i < num_teams; i++) 
+			fn.addEdge(new FlowEdge(i, num_teams + 1, w[x] + r[x] - w[i]));
+
+		//source at index number = num_teams and sink at index number = num_team + 1 
+		FordFulkerson ff = new FordFulkerson(fn, num_teams, num_teams + 1);
+	
+		k = 0;
+		for(int i = 0; i < num_teams; i++)
+			if (i != x && ff.inCut(i)) {
+				rset.add(teams[i]);
+				k++;
+			}
+		
+		if (k != 0) {
+			peliminated = true;
+			return true;
+		}
+
+		return false;
+	}
+	
+	public Iterable<String> certificateOfElimination(String team) {
+		if (team.equals(prev_team))
+			if(rset.isEmpty())
+				return null;
+			else
+				return rset; 
+		
+		int x = -1;
+		for (int i = 0; i < numberOfTeams(); i++)
+			if (teams[i].equals(team))
+				x = i;
+		
+		if (x == -1)
+			throw new java.lang.IllegalArgumentException("No such team");
+
+		this.rset = new HashSet<String>();
+		prev_team = team;
+		peliminated = false;
+		
+		for (String t : teams)
+			if (w[x] + r[x] < wins(t)) {
+				rset.add(t);
+				peliminated = true;
+			}
+	
+		if (peliminated)			
+			return rset;
+		
+		
+		int v = 2 + num_teams;		//track number of vertices
+		for(int i = 0; i < num_teams; i++) 
+			for (int j = i + 1; j < num_teams; j++)
+				if (g[i][j] != 0)
+					v++;
+		
+		FlowNetwork fn = new FlowNetwork(v);
+		
+		//connect start to games to teams
+		int k = 0;
+		for(int i = 0; i < num_teams; i++) 
+			for (int j = i + 1; j < num_teams; j++)
+				if (g[i][j] != 0) {
+					fn.addEdge(new FlowEdge(num_teams, num_teams + k + 2, g[i][j]));
+					fn.addEdge(new FlowEdge(num_teams + k + 2, i, Double.POSITIVE_INFINITY));
+					fn.addEdge(new FlowEdge(num_teams + k + 2, j, Double.POSITIVE_INFINITY));
+					k++;
+				}
+		
+		//connect teams to end
+		for(int i = 0; i < num_teams; i++) 
+			fn.addEdge(new FlowEdge(i, num_teams + 1, w[x] + r[x] - w[i]));
+			
+		FordFulkerson ff = new FordFulkerson(fn, num_teams, num_teams + 1);
+	
+		k = 0;
+		for(int i = 0; i < num_teams; i++)
+			if (i != x && ff.inCut(i)) {
+				rset.add(teams[i]);
+				k++;
+			}
+		
+		if (k != 0) {
+			peliminated = true;
+			return rset;
+		}
+		
+		// subset R of teams that eliminates given team; null if not eliminated
+		return null;
+	}
+}
