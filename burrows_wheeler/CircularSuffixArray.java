0e5277286c92b8ea887411c4db79c9089d2433dc
Edward Tseng <axlinkck@aol.com>
2014-02-11 18:33:19 -0800

    Update CircularSuffixArray.java

--- burrows_wheeler/CircularSuffixArray.java
+++ burrows_wheeler/CircularSuffixArray.java
@@ -16,19 +16,8 @@ public class CircularSuffixArray {
         for (int i = 0; i < N; i++)
         	next[i] = i;
         CUTOFF = s.length() - 2;
-	
-        //lsd(suffixes, N);
+        
     	msd(s);
-    	//mergeSort(suffixes, 0);
-    	//insertion(s, 0, N-1, 0, next);
-    	//quickSort(s, 0, N-1, 0);
-    	
-    	
-
-        //for (int i = 0; i < N; i++)
-        	//System.out.println(next[i]);
-        	//next[i] = (int) (suffixes[i][N] - '0');
-    	
     }
     
     public int length() {
@@ -59,10 +48,7 @@ public class CircularSuffixArray {
     private void sort(String a, int lo, int hi, int d, int[] aux) {
         // cutoff to insertion sort for small subarrays
         if (hi <= lo + CUTOFF) {
-    	//if (true) {
-        	//mergeSort(a, d);
-        	//System.out.println("QUICK");
-        	quickSort(a, lo, hi, d);
+    		quickSort(a, lo, hi, d);
             return;
         }
                
@@ -90,16 +76,6 @@ public class CircularSuffixArray {
         for (int i = lo; i <= hi; i++) { 
         	next[i] = aux[i - lo];
         }
-       /*
-        System.out.println("MSD");
-        System.out.print("\n");
-        System.out.print("\n");
-        for (int g = 0; g < a.length(); g++) {
-        	for (int e = 0; e < a.length(); e++)
-        		System.out.print(suffixfn(a, next[g], e, a.length()));
-        	System.out.print("\n");
-        }*/
-        
         // recursively sort for each character
         for (int r = 0; r < R; r++)
         	if (count [r+1] - count[r] > 1 && d < n)
@@ -135,29 +111,11 @@ public class CircularSuffixArray {
 
     // 3-way string quicksort a[lo..hi] starting at dth character
     private void quickSort(String a, int lo, int hi, int d) { 
-
-    	//if (hi <= lo) return;
         // cutoff to insertion sort for small subarrays
         if (hi <= lo + 5 ) {
-    	//if (false) {
-        	//System.out.println("INSERT");
-        	//System.out.println(" D: " + d + " lo " + lo + " hi " + hi);
-            insertion(a, lo, hi, d, next);
+    	    insertion(a, lo, hi, d, next);
             return;
         }
-        
-    	/*System.out.print("BEFORE");System.out.print("\n");
-    	System.out.println("lo: " + lo + " hi: " + hi + " d: " + d);
-        System.out.print("\n");
-        System.out.print("\n");
-        for (int g = 0; g < a.length(); g++) {
-        	for (int e = 0; e < a.length(); e++)
-        		System.out.print(suffixfn(a, next[g], e, a.length()));
-        	System.out.print("\n");
-        }
-        
-        System.out.print("\n");System.out.print("\n");
-        */
         
         int n = a.length();
         int lt = lo, gt = hi;
@@ -171,20 +129,6 @@ public class CircularSuffixArray {
             else              i++;
         }
 
-        /*System.out.print("AFTER");System.out.print("\n");
-    	System.out.println("lo: " + lo + " hi: " + hi + " d: " + d);
-        System.out.print("\n");
-        System.out.print("\n");
-        for (int g = 0; g < a.length(); g++) {
-        	for (int e = 0; e < a.length(); e++)
-        		System.out.print(suffixfn(a, next[g], e, a.length()));
-        	System.out.print("\n");
-        }
-        
-        System.out.print("\n");System.out.print("\n");*/
-
-//        System.out.println("next: " + next[i] + " D: " + d + " n: " + n + " lo " + lo + " hi " + hi);
-        // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]. 
         quickSort(a, lo, lt-1, d);
         if (v >= 0 && d < n) quickSort(a, lt, gt, d+1);
         quickSort(a, gt+1, hi, d);
@@ -193,18 +137,16 @@ public class CircularSuffixArray {
     
     
     public static void main(String[] args){    	
-    	Stopwatch sw = new Stopwatch();
-    	CircularSuffixArray a;
-    	//for(int i = 0; i<100000; i++)
-    		a = new CircularSuffixArray("couscousadfajaabbascpojpoadsf;ljasldf");
-    		//a = new CircularSuffixArray("CADABRA!ABRA");
+    	// Stopwatch sw = new Stopwatch();
+    	// CircularSuffixArray a;
+    	// //for(int i = 0; i<100000; i++)
+    	// 	a = new CircularSuffixArray("couscousadfajaabbascpojpoadsf;ljasldf");
+    	// 	//a = new CircularSuffixArray("CADABRA!ABRA");
     	
     	
-    	System.out.println("Resizing time: " + sw.elapsedTime() + " seconds.");
-    	for (int i = 0; i < a.length(); i++)
-    		System.out.print(a.index(i));
-    	//11107035814692
-    	
+    	// System.out.println("Resizing time: " + sw.elapsedTime() + " seconds.");
+    	// for (int i = 0; i < a.length(); i++)
+    	// 	System.out.print(a.index(i));
     	
     }
 }
